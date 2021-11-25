66b613102be33629d1b399509123df6d821ebe16
irfanfiriezky <irfanfiriezkybahri@gmail.com>
2021-11-25 12:13:29 +0700

    Test Repo

--- app/src/main/java/id/ac/umn/jophoto/MainActivity.java
+++ app/src/main/java/id/ac/umn/jophoto/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Test Repository
+    //Repo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
